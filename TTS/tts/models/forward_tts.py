8f4bf0b407998aa70f9f73d365ce992bb4a5ad8a
Kevinsweep <110760187+Kevinsweep@users.noreply.github.com>
2025-05-14 10:29:39 +0700

    Update forward_tts.py
    set avg_pitch and avg_energy to None

--- TTS/tts/models/forward_tts.py
+++ TTS/tts/models/forward_tts.py
@@ -639,6 +639,7 @@ class ForwardTTS(BaseTTS):
         avg_pitch = None
         if self.args.use_pitch:
             o_pitch_emb, o_pitch = self._forward_pitch_predictor(o_en, x_mask, pitch, dr)
+            avg_pitch = None		
             o_en = o_en + o_pitch_emb
         # energy predictor pass
         o_energy = None
@@ -646,6 +647,7 @@ class ForwardTTS(BaseTTS):
         if self.args.use_energy:
             o_energy_emb, o_energy = self._forward_energy_predictor(o_en, x_mask, energy, dr)
             o_en = o_en + o_energy_emb
+	    avg_energy = None
         # decoder pass
         o_de, attn = self._forward_decoder(
             o_en, dr, x_mask, y_lengths, g=None
